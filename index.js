097ad285bc1f8d15f04453b1f0ff5586bd2f447f
phamleduy04 <32657584+phamleduy04@users.noreply.github.com>
2020-02-10 18:43:54 -0600

    Update trigger words

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs')
 var array = fs.readFileSync('thinh.txt','utf8').split("\n")
 
  
-client.login(KEY);
+client.login("NjY2MzYwNjUxMTkwNjMyNDU5.Xh3tfA.2d3FjeeODgCIa1_B7aB8DlZ9VnE");
 
 // Set the bot's presence (activity and status)
 client.on("ready", () => {
@@ -15,14 +15,15 @@ client.on("ready", () => {
         },
         status: 'Online'
     })
-    console.log("Ready!")
+    console.log(`${client.user.username} is ready!`)
 })
 //thính thính =)))
 client.on("message", (message) => {
-    let content = ["xin thính", "thính", "thính ơi", "xin thinh"]
+    let content = ["xin thính", "thính", "thính ơi", "xin thinh","úm ba la xì bùa cho con cái thính","thinh","xin thinh"]
     if (content.includes(message.content.toLowerCase())) {
+        if(message.author.id == '479503913565945856') 
+            return message.reply(`Học không lo học suốt ngày thính cc `).then(m => m.delete(5000))
         const random = array[Math.floor(Math.random() * array.length)]
         message.channel.send(random)
-        
     }   
 })
